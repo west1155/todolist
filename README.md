b41acbb8c38a4189797b06f44dc8b029ef7d253b
Aleksandr Leimberg <aleimberg@west.local>
2023-10-28 17:36:30 +0100

    added material UI

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # todoList
+
+
+
+1. Simple To do List realisation
+2. Material UI
+3. add remove and edit functionality
+4. shows the list after browser restarted
+5. couple of tests
+6. README
+
--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React, {useState} from 'react' 
 import TodoForm from "./TodoForm";
+import { Container, TextField, Button, Typography } from '@mui/material';
 
 function App() {
   
   
   const [inputTask, setInput] = useState('')
-  const [tasks, setTasks] = useState(['dasdfe','dfwetrg'])
+  const [tasks, setTasks] = useState([''])
   
   const addTask = (input) => {
     if (input !== null && input !== '') {
@@ -17,17 +18,26 @@ function App() {
   };
   
   return (
-    <div className="App">
-      <div className="App-header">
-          <input type='text'
+    <Container maxWidth="sm" className="app-container">
+      <Typography variant="h4" component="h1" className="app-heading">
+        To Do List
+      </Typography>
+    <div className="todo-container">
+      <div className="todo-app">
+          <TextField
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            className="todo-input"
             placeholder={"Input text"}
             value={inputTask}
             onChange={(e) => setInput(e.target.value)}/>
-          <button type='submit' onClick={addTask} >Add Task</button>
+        <Button variant="contained" color="primary" className="add-button" onClick={addTask} >Add Task</Button>
         <TodoForm tasks={tasks}/>
       </div>
       
     </div>
+    </Container>
   );
 }
 
